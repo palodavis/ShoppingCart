c28ec19e6367932ceb8d744beca05104ecfb5e8b
palodavis <psilvadias59@gmail.com>
2025-01-07 19:10:03 -0300

    fix: Updating sql file

--- database_shoppingCart.sql
+++ database_shoppingCart.sql
@@ -1,7 +1,16 @@
-CREATE TABLE product (
-	id_product INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
+CREATE TABLE product
+(
+    id_product INT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(255) NOT NULL,
+    category   VARCHAR(255) NOT NULL,
     price DOUBLE NOT NULL,
-    amount INT NOT NULL
+    amount     INT          NOT NULL
+);
+
+CREATE TABLE shoppingCart
+(
+    id_shopping INT PRIMARY KEY AUTO_INCREMENT,
+    amount      INT NOT NULL,
+    product_id  INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product (id_product)
 );
