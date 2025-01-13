968ce661d4613937efdf78aa0db7a0f385e85b00
palodavis <psilvadias59@gmail.com>
2025-01-13 10:39:22 -0300

    fix: add information to sql

--- database_shoppingCart.sql
+++ database_shoppingCart.sql
@@ -7,6 +7,10 @@ CREATE TABLE product
     amount INT NOT NULL
 );
 
+CREATE TABLE cart
+(
+    id_cart INT PRIMARY KEY AUTO_INCREMENT
+);
 
 CREATE TABLE shoppingCart
 (
@@ -19,7 +23,9 @@ CREATE TABLE shoppingCart
     FOREIGN KEY (product_id) REFERENCES product (id_product)
 );
 
-CREATE TABLE cart
-(
-    id_cart INT PRIMARY KEY AUTO_INCREMENT
-);
+INSERT INTO product (name, category, price, amount)
+VALUES ("Notebook", "Eletronics", 5000, 20),
+       ("Motheboard", "Eletronics", 1500, 10),
+       ("Smartphone", "Eletronics", 3000, 25),
+       ("Headphones", "Eletronics", 300, 50),
+       ("Tablet", "Eletronics", 2000, 15);
